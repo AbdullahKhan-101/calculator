97390f4101626d2e909958d26d61a33ea807fac3
abdullahkhan44 <86144086+abdullahkhan44@users.noreply.github.com>
2021-09-19 09:29:19 -0700

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-function getNumber(num) {
-  var result = document.getElementById("result");
-  result.value += num;
-}
-function finish() {
-  var result = document.getElementById("result");
-  result.value = "";
+var min = 0;
+var sec = 0;
+var msec = 0;
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+
+var interval;
+
+function timer() {
+  msec++;
+  msecHeading.innerHTML = msec;
+  if (msec >= 100) {
+    sec++;
+    secHeading.innerHTML = sec;
+    msec = 0;
+  } else if (sec >= 60) {
+    min++;
+    minHeading.innerHTML = min;
+    sec = 0;
+  }
 }
 
-function getResult() {
-  var result = document.getElementById("result");
-  result.value = eval(result.value);
+function start() {
+  interval = setInterval(timer, 10);
+  var btn = document.getElementById("dis");
+  btn.disabled = "off";
+}
+
+function stop() {
+  clearInterval(interval);
+}
+
+function reset() {
+  min = 0;
+  sec = 0;
+  msec = 0;
+  minHeading.innerHTML = min;
+  secHeading.innerHTML = sec;
+  msecHeading.innerHTML = msec;
+  stop();
 }
